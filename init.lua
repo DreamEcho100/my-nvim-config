0f5b4bae8a03b77e591df9ae246090ab850b2808
Mazen Mohamed Shaban <mazen.mohamed@viavisolutions.com>
2025-02-08 15:47:08 +0200

    add telescope and lsp plugins to configuration

--- init.lua
+++ init.lua
@@ -31,5 +31,7 @@ require('lazy').setup({{
     require 'plugins/bufferline',
     require 'plugins/lualine',
     require 'plugins/treesitter',
+    require 'plugins/telescope',
+    require 'plugins/lsp'
 }})
 
--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -13,17 +13,19 @@ return {
 
         -- Load the colorscheme
         require('nord').set()
-        
 
-    -- Toggle background transparency
-    local bg_transparent = false
+        -- Toggle background transparency
+        local bg_transparent = false
 
-    local toggle_transparency = function()
-      bg_transparent = not bg_transparent
-      vim.g.nord_disable_background = bg_transparent
-      vim.cmd [[colorscheme nord]]
-    end
+        local toggle_transparency = function()
+            bg_transparent = not bg_transparent
+            vim.g.nord_disable_background = bg_transparent
+            vim.cmd [[colorscheme nord]]
+        end
 
-    vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>bg', toggle_transparency, {
+            noremap = true,
+            silent = true
+        })
     end
 }
